1105823eca7ed4ecd76bde576946e3d3b2fc44a4
phuctran <hp.sliver123@gmail.com>
2023-05-03 19:49:02 +0700

    change url

--- src/untils/callAPI.js
+++ src/untils/callAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default function callApi(endpoint, method = 'GET', body, header) {
   return axios({
     method: method,
-    url: `https://book-store-api-ashy.vercel.app/api${endpoint}`,
+    url: `https://book-store-api-moyy.onrender.com/api${endpoint}`,
     // url: `http://localhost:5000/api${endpoint}`,
     data: body,
     headers: header
