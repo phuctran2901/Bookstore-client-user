35ccf34603a68731008c8c63b98fa2dfab7da1f9
phuctran <hp.sliver123@gmail.com>
2023-05-05 10:15:26 +0700

    fix featured book

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,46 +1,32 @@
-
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Header } from '../../components/Header/index';
-import { CarouselP } from './Carousel/index';
-import { SearchBar } from '../../components/SearchBar/index';
-import { Category } from '../../components/Category/index';
-import { Featured } from './Featured/index';
-import { Sale } from './Sale/index';
-import { Maxim } from './Maxim/index';
-import { Introduce } from './Intro/index';
-import { Blog } from './Blog/index';
-import { Footer } from '../../components/Footer/index';
-import { useSelector, useDispatch } from "react-redux";
-
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Header } from '../../components/Header/index'
+import { CarouselP } from './Carousel/index'
+import { SearchBar } from '../../components/SearchBar/index'
+import { Category } from '../../components/Category/index'
+import { Featured } from './Featured/index'
+import { Sale } from './Sale/index'
+import { Maxim } from './Maxim/index'
+import { Introduce } from './Intro/index'
+import { Blog } from './Blog/index'
+import { Footer } from '../../components/Footer/index'
+import { useSelector, useDispatch } from 'react-redux'
 
 export const Home = () => {
-    const products = useSelector(state => state.products);
-    const posts = useSelector(state => state.posts.postAll) || [];
-    return (
-        <div>
-            <Header />
-            <CarouselP />
-            <SearchBar />
-            <Category
-                products={products.productsPopular}
-                label="Popular Books"
-            />
-            <Featured
-                product={products.listProduct[10]}
-            />
-            <Sale
-                products={products.productsNews}
-            />
-            <Maxim />
-            <Introduce />
-            <Category
-                products={products.productsSale}
-                label="Sale Books"
-            />
-            <Blog
-                posts={posts}
-            />
-            <Footer />
-        </div>
-    )
+  const products = useSelector((state) => state.products)
+  const posts = useSelector((state) => state.posts.postAll) || []
+  return (
+    <div>
+      <Header />
+      <CarouselP />
+      <SearchBar />
+      <Category products={products.productsPopular} label='Popular Books' />
+      <Featured product={products.listProduct[1]} />
+      <Sale products={products.productsNews} />
+      <Maxim />
+      <Introduce />
+      <Category products={products.productsSale} label='Sale Books' />
+      <Blog posts={posts} />
+      <Footer />
+    </div>
+  )
 }
